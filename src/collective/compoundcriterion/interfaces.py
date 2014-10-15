42b5ba61b60e4c6005e8a40fabb8311b036459ad
Gauthier Bastien <g.bastien@imio.be>
2014-10-15 11:11:42 +0200

    Added workable prototype before adapting it

--- src/collective/compoundcriterion/interfaces.py
+++ src/collective/compoundcriterion/interfaces.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class ICollectiveCompoundcriterionLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class ICoumpondCriterionFilter(Interface):
+    """Marker interface for the adapter to use as compound criterion possible value."""
