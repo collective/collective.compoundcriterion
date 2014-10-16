b52eec28844a4183bbe9cfa63b6cb1ccccbc31d9
Gauthier Bastien <g.bastien@imio.be>
2014-10-16 13:38:21 +0200

    Defined vocabulary used in the compound criterion widget

---
+++ src/collective/compoundcriterion/vocabularies.py
@@ -0,0 +1,21 @@
+from zope.component import getGlobalSiteManager
+from zope.interface import implements
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleTerm
+from collective.compoundcriterion.interfaces import ICompoundCriterionFilter
+
+
+class CompoundCriterionVocabulary(object):
+    """Vocabulary factory for compound criterion.
+       This will return every named adapter that provides the ICoumpondCriterionFilter interface."""
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        sm = getGlobalSiteManager()
+        registrations = [a for a in sm.registeredAdapters() if a.provided == ICompoundCriterionFilter]
+        terms = [SimpleTerm(adapter.name, adapter.name, adapter.name) for adapter in registrations]
+        return SimpleVocabulary(terms)
+
+
+CompoundCriterionVocabularyFactory = CompoundCriterionVocabulary()
