5d3f43415b99809dda91e8ff7ca751e70d84909a
Stephan Geulette <s.geulette@imio.be>
2015-04-09 12:32:30 +0200

    Criterion label can be translated

--- src/collective/compoundcriterion/vocabularies.py
+++ src/collective/compoundcriterion/vocabularies.py
@@ -4,6 +4,7 @@ from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
 from zope.schema.vocabulary import SimpleTerm
 from collective.compoundcriterion.interfaces import ICompoundCriterionFilter
+from Products.CMFPlone import PloneMessageFactory as _p
 
 
 class CompoundCriterionVocabulary(object):
@@ -14,7 +15,7 @@ class CompoundCriterionVocabulary(object):
     def __call__(self, context):
         sm = getGlobalSiteManager()
         registrations = [a for a in sm.registeredAdapters() if a.provided == ICompoundCriterionFilter]
-        terms = [SimpleTerm(adapter.name, adapter.name, adapter.name) for adapter in registrations]
+        terms = [SimpleTerm(adapter.name, adapter.name, _p(adapter.name)) for adapter in registrations]
         return SimpleVocabulary(terms)
 
 
